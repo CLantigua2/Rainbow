9bc27a042d94eabb57ce828d75ee2d4f59235e21
Clantigua2 <clantigua954@gmail.com>
2019-05-09 15:21:27 -0400

    completely added 6 background color changing buttons and tested functionality

--- Rainbow/Rainbow/ViewController.swift
+++ Rainbow/Rainbow/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func purpleButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .purple
+    }
+    
+    @IBAction func orangeButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .orange
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .green
+    }
+    
+    @IBAction func redButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .red
+    }
+    @IBAction func blueButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .blue
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        self.view.backgroundColor = .yellow
+    }
 }
 
